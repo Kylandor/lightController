f264e9fdaf025deaa93710ed7b2bd22dc33d3bf6
Kylandor <kovarikkj@gmail.com>
2020-12-20 12:05:41 -0600

    Update display.py

--- display.py
+++ display.py
@@ -1,1 +1,10 @@
-print("im sorry for what i said earlier")
+import board
+import neopixel
+
+print("displaying...")
+
+NUM_LEDS = 150
+pixels = neopixel.NeoPixel(board.D18, NUM_LEDS)
+
+pixels.fill((0, 255, 0))
+pixels.show()
