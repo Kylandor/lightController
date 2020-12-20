8b05c3d7c0f6dadfe4395b0e8a80c77686d29dab
Kylandor <kovarikkj@gmail.com>
2020-12-20 12:31:04 -0600

    Update display.py

--- display.py
+++ display.py
@@ -9,9 +9,11 @@ def exit_handler():
 atexit.register(exit_handler)
 
 print("displaying...")
-
+ORDER = neopixel.GRB
 NUM_LEDS = 150
-pixels = neopixel.NeoPixel(board.D18, NUM_LEDS, 0.8)
+pixels = neopixel.NeoPixel(
+    board.D18, NUM_LEDS, brightness=0.8, auto_write=False, pixel_order=ORDER
+)
 
 while True:
     pixels.fill((0, 0, 255))
