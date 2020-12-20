0d3ce6c88eff3ed706d441bf1bbf89f70ff520a9
Kylandor <kovarikkj@gmail.com>
2020-12-20 12:17:52 -0600

    Update display.py

--- display.py
+++ display.py
@@ -6,5 +6,6 @@ print("displaying...")
 NUM_LEDS = 150
 pixels = neopixel.NeoPixel(board.D18, NUM_LEDS)
 
-pixels.fill((0, 0, 255))
-pixels.show()
+while True:
+    pixels.fill((0, 0, 255))
+    pixels.show()
