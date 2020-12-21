68eeb3af5734b3e1a63d9baf10a3fe69664c724a
Kylandor <kovarikkj@gmail.com>
2020-12-21 17:38:50 -0600

    new frames

--- addFrameServer.py
+++ addFrameServer.py
@@ -6,18 +6,26 @@ app = Flask(__name__)
 def home():
     f = open('frames', 'ab')
     frame = []
+    numberOfSec = 0
     data = request.data
     data = data.decode("utf-8")
+    numberOfSec = int(data[data.index("sec=")+4:])
     data = data.split("rgb")
     data = data[1:]
     for i in data:
         cur = i[1:i.index(")")]
         cur = cur.split(", ")
         frame.append((int(cur[0]), int(cur[1]), int(cur[2])))
+    print(numberOfSec)
     frameData = createData.makePacket(frame)
+    frameData = frameData * (22 *numberOfSec)
     frameData.tofile(f)
     f.close()
     return "ok"
+@app.route("/clear", methods = ['GET'] )
+def clearFrame():
+    open("frame", 'w').close()
+    return "cleared"
 
 if __name__ == '__main__':
     app.debug = True
